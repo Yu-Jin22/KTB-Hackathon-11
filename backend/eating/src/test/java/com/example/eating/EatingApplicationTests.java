aca82573c424cd7409fad4eccc18b69b9d361128
kaa08 <kaa08@naver.com>
2025-12-17 13:32:20 +0900

    ktb-11-hackathon

---
+++ backend/eating/src/test/java/com/example/eating/EatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
